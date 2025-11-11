b34a3cc0c9abda5b9097d81c8accc5b4aa28308f
DCFInvest <rajivkanumilli@gmail.com>
2025-11-11 16:55:28 -0500

    Create App component for AI Stock Advisor

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import ChatUI from "./ChatUI";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>AI Stock Advisor</h1>
+      <ChatUI />
+    </div>
+  );
+}
+
+export default App;
